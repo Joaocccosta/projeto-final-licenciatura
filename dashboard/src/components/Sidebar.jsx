c5c39c15f320215e0595e57013dd56e7fab0775c
João Costa <169092712+Joaocccosta@users.noreply.github.com>
2025-04-23 21:47:05 +0100

    sidebar component a funcionar, problema do botão resolvido

--- dashboard/src/components/Sidebar.jsx
+++ dashboard/src/components/Sidebar.jsx
@@ -1,25 +1,20 @@
 import { ChevronLeft, ChevronRight } from "lucide-react";
 
-const Sidebar = ({ visible, toggle }) => {
-  return (
-    <div 
-      className={`relative transition-all duration-300 h-full ${visible ? 'w-64' : 'w-0'} overflow-hidden`}
-    >
-      {/* Sidebar container */}
-      <div className="absolute top-0 left-0 w-64 h-full bg-black shadow p-4 text-white">
-        {/* Sidebar content */}
-        <p>Sidebar</p>
-      </div>
-      
-      {/* Toggle button - attached to the sidebar edge */}
-      <button
-        onClick={toggle}
-        className="absolute top-10 -ml-0 bg-white text-gray-700 rounded-r p-1 shadow-md z-10"
-      >
-        {visible ? <ChevronLeft /> : <ChevronRight />}
+const Sidebar = ({ visible, toggle }) => (
+  <div className={`transition-all duration-300 ${visible ? "w-75" : "w-0"}  bg-gray-800 text-white border-l shadow h-full relative`}>
+    {/* Botão visível sempre, logo abaixo do topo da sidebar */}
+    <div className="p-2">
+      <button onClick={toggle} className="absolute top-1/2 -translate-y-1/2 -left-10 bg-white text-white rounded-r px-1 py-2 shadow z-10">
+        {visible ? <ChevronRight size={16} /> : <ChevronLeft size={16} />}
       </button>
     </div>
-  );
-};
+
+    {visible && (
+      <div className="p-4">
+        <p className="text-sm">Sidebar</p>
+      </div>
+    )}
+  </div>
+);
 
 export default Sidebar;
