0c29143a57792e09f3a05fee37103c5909661836
João Costa <169092712+Joaocccosta@users.noreply.github.com>
2025-05-05 13:26:32 +0100

    ok

--- api/db.js
+++ api/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'your_password',
-  database: 'your_database',
+  password: 'root',
+  database: 'producao_indicadores_teste',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
